32f2c87ffa1f5971e4af1ca930f40f474b7947ef
ZuhairHisbani789 <115968488+ZuhairHisbani789@users.noreply.github.com>
2023-05-24 06:03:57 -0700

    Add files via upload

---
+++ atm.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class ATM {
+    private static double balance = 1000; // Initial account balance
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("ATM MENU");
+            System.out.println("1. Check Balance");
+            System.out.println("2. Withdraw Cash");
+            System.out.println("3. Deposit Cash");
+            System.out.println("4. Quit");
+            System.out.print("Enter your choice: ");
+            int choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    checkBalance();
+                    break;
+                case 2:
+                    System.out.print("Enter the amount to withdraw: ");
+                    double withdrawAmount = input.nextDouble();
+                    withdraw(withdrawAmount);
+                    break;
+                case 3:
+                    System.out.print("Enter the amount to deposit: ");
+                    double depositAmount = input.nextDouble();
+                    deposit(depositAmount);
+                    break;
+                case 4:
+                    System.out.println("Thank you for using the ATM. Goodbye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
+    }
+
+    private static void checkBalance() {
+        System.out.println("Your balance is: $" + balance);
+    }
+
+    private static void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient balance. Withdrawal failed.");
+        } else {
+            balance -= amount;
+            System.out.println("Successfully withdrew $" + amount);
+            System.out.println("Remaining balance: $" + balance);
+        }
+    }
+
+    private static void deposit(double amount) {
+        balance += amount;
+        System.out.println("Successfully deposited $" + amount);
+        System.out.println("Updated balance: $" + balance);
+    }
+}
